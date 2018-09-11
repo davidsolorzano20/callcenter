2e3ab6a2c92de7fbc83e975464231c33a334e354
Luis Solorzano <luisnic20@gmail.com>
2018-09-11 09:41:25 -0600

    Dialer

--- app/src/main/java/com/thunderlabs/callcenter/MainActivity.java
+++ app/src/main/java/com/thunderlabs/callcenter/MainActivity.java
@@ -1,19 +1,31 @@
 package com.thunderlabs.callcenter;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private final int PERMISSION_REQUEST_CONTACT = 2;
+    private static final int PERMISSIONS_REQUEST_SEND_SMS = 0;
+    private Context mContext = MainActivity.this;
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,14 +33,30 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_sms:
+                    if (Build.VERSION.SDK_INT >= 23) {
+                        String[] PERMISSIONS = {Manifest.permission.SEND_SMS};
+                        if (!hasPermissions(mContext, PERMISSIONS)) {
+                            ActivityCompat.requestPermissions((Activity) mContext, PERMISSIONS, PERMISSIONS_REQUEST_SEND_SMS);
+                        } else {
+                            sendSMS();
+                        }
+                    } else {
+                        sendSMS();
+                    }
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_contact:
+
+                    if (Build.VERSION.SDK_INT >= 23) {
+                        String[] PERMISSIONS = {Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS};
+                        if (!hasPermissions(mContext, PERMISSIONS)) {
+                            ActivityCompat.requestPermissions((Activity) mContext, PERMISSIONS, PERMISSION_REQUEST_CONTACT);
+                        } else {
+                            addContacts();
+                        }
+                    } else {
+                        addContacts();
+                    }
                     return true;
             }
             return false;
@@ -40,9 +68,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+    private void addContacts() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        Intent contactIntent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        contactIntent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+        //Por si quieres ingresar el nombre
+        contactIntent.putExtra(ContactsContract.Intents.Insert.NAME, "Luis Solorzano");
+
+        //Add number
+        ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
+        ContentValues phoneRow = new ContentValues();
+        phoneRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        phoneRow.put(ContactsContract.CommonDataKinds.Phone.NUMBER, "55025936");
+        contactData.add(phoneRow);
+        contactIntent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
+        startActivity(contactIntent);
+    }
+
+    private void sendSMS() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        String smsNumber = "55025936";
+        String sms = "que paso";
+
+        Uri uri = Uri.parse("smsto:"+smsNumber);
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+        smsIntent.putExtra("sms_body", sms);
+        startActivity(smsIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CONTACT: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    addContacts();
+                } else {
+                    Toast.makeText(mContext, "La aplicación no pudo guardar el contacto.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            case PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendSMS();
+                } else {
+                    Toast.makeText(mContext, "La aplicación no pudo guardar el contacto.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
